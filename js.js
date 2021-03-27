638c62b64fd60597158f27236ce85092e494ebf2
Abdullah-Sheehan <62787649+Abdullah-Sheehan@users.noreply.github.com>
2021-03-27 16:09:10 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+let x = document.getElementById('ttt');
+document.addEventListener("click", function(){
+  x.classList.toggle("vis");
+});
+function myFunction1(){
+    document.getElementById('hel').innerHTML = "X";
+}
+function myFunction2(){
+    document.getElementById('hel').innerHTML = "A";
+}
+function myFunction3(){
+    document.getElementById('hel').innerHTML = "An";
+}
+function myFunction4(){
+    document.getElementById('hel').innerHTML = "The";
+}
